84f683e55a0bc7645b57d1f32705e665e6d90623
Vineet Bharot <vbharot@circinus-18.ics.uci.edu>
2021-03-16 12:15:53 -0700

    error echoing for adversary

--- create-trades.sh
+++ create-trades.sh
@@ -6,13 +6,13 @@ MONTH_2_DIR=$3
 TMP_DIR=$4
 
 if [[ $HTML_FORMAT = 2001 || $HTML_FORMAT = 2016 ]] ; then
-    echo $HTML_FORMAT "stocks data profiles doesn't have enough inforamation like quarterly values of various field which are needed to make f,g and magic_score values"
-    exit
+    echo -e "\nHTML FORMAT NOT SUPPORTED:" $HTML_FORMAT "stocks data profiles doesn't have enough inforamation like quarterly values of various field which are needed to make f,g and magic_score values. \n" 1>&2
+    exit 1
 fi
 
 if [ $# -lt 4  ] ; then
-    echo "usage: create-trades.sh html_profiles month_1 month_1 tmp_dir_path"
-    exit
+    echo "usage: create-trades.sh html_profiles month_1 month_1 tmp_dir_path" 1>&2
+    exit 1
 fi
 
 MONTH_1=$(basename $MONTH_1_DIR)
@@ -26,3 +26,5 @@ if $CRAWS_RES ; then
     cd ../
     python trade.py $HTML_FORMAT $MONTH_1 $MONTH_2 $TMP_DIR $STOCKS_DATA_DIR
 fi
+
+cd $STOCKS_DATA_DIR
