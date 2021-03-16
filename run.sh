18b5a1b6f10cb97b7362927a587a3426f9ad6789
Vineet Bharot <vbharot@circinus-18.ics.uci.edu>
2021-03-16 09:13:58 -0700

    activating venve in run.sh

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ MONTH_1=$(basename $MONTH_1_DIR)
 MONTH_2=$(basename $MONTH_2_DIR)
 TMP_DIR=$4
 cd ${CODE_DIR%run.sh}/tmp/scrapy_tutorials/scrapy_tutorials
+source ${CODE_DIR%run.sh}/venvs/tutorials/bin/activate
 scrapy crawl profiles_$HTML_FORMAT -a html_format=$HTML_FORMAT -a month_1=$MONTH_1 -a month_2=$MONTH_2 -a tmp_dir=$TMP_DIR -a stocks_data_dir=$STOCKS_DATA_DIR --overwrite-output=$TMP_DIR/stocks_data_$HTML_FORMAT-$MONTH_2.jsonlines --loglevel=ERROR
 cd ../../../
 python trade.py $HTML_FORMAT $MONTH_1 $MONTH_2 $TMP_DIR $STOCKS_DATA_DIR
